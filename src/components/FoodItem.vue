ec2497a6ae00bb1099c7a7aa32f0683adfac21e7
rnaneva <ralitsananeva@gmail.com>
2023-11-21 23:26:59 +0200

    components and props

---
+++ src/components/FoodItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h2>
+            {{ foodName }}
+            <img src="/img_quality.svg" v-show="isFavourite">
+        </h2>
+        <p>{{ foodDesc }}</p>
+     
+    </div>
+</template>
+<script>
+export default{
+     // props: ['foodName','foodDesc','isFavorite']
+  props: {
+    foodName: {
+        type: String,
+        required: true
+  },
+    foodDesc: {
+        type: String,
+        required: false,
+        default:  'This is the default description.',
+        validator: function(value){
+            if(20 < value.length && value.length < 40){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    isFavourite: {
+        type: Boolean,
+        required: false,
+        default: false
+    } 
+  },
+  data(){
+    return {
+        foodIsFavourite: this.isFavourite
+    }
+  },
+  methods: {
+    toggleFavourite(){
+        this.foodIsFavourite = !this.foodIsFavourite;
+    }
+  }
+}
+</script>
+<style>
+ #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }
+  img{
+    height:1.5em;
+    float: right;
+  }
+  </style>
