2a0e6f0e11f90d799e4c9edf0b4c896103c00014
rnaneva <ralitsananeva@gmail.com>
2023-11-26 13:29:56 +0200

    template refs

---
+++ src/App8.vue
@@ -0,0 +1,43 @@
+
+<template>
+    
+    <h1>Example 1</h1>
+    <p>Write something and it will be repeated below</p>
+    <input ref="inputEl" @input="getRefs" placeholder="Write something...">
+    <p ref="pEl" ></p>
+
+    <h1>Example 2</h1>
+    <p>Click the button to reveal the 3rd list element stored as an array element in the $refs object.</p> 
+    <button @click="getValue">Get the 3rd list element</button><br>
+    <ul>
+      <li v-for="x in liTexts" ref="liEl">{{ x }}</li>
+    </ul>
+    <pre>{{ thirdEl }}</pre>
+  </template>
+  
+  <script>
+  export default{
+    data(){
+          return{
+            thirdEl: '',
+            liTexts:  ['Apple','Banana','Kiwi','Tomato','Lichi']
+          }
+        },
+      methods:{
+          getRefs(){
+            this.$refs.pEl.innerHTML = this.$refs.inputEl.value
+          },
+          getValue(){
+            this.thirdEl = this.$refs.liEl[2].innerHTML;
+            
+          }
+      }
+  };
+  </script>
+  
+  <style>
+  #pEl {
+    background-color: lightgreen;
+    display: inline-block;
+  }
+  </style>   
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 import { createApp } from 'vue'
-import App from './App7.vue'
+import App from './App8.vue'
 
 const app = createApp(App)
 app.mount('#app')
