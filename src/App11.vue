5fd3df2cdd268f04bcea6cf1d72cfdbe0109a8d1
rnaneva <ralitsananeva@gmail.com>
2023-11-27 21:44:20 +0200

    form inputs

---
+++ src/App11.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Input Type Color</h1>
+     <form @submit.prevent="registerAnswer">
+        <label>Chose a color<br>
+            <input v-model="colorInp" type="color" >
+        </label>
+        
+        <button type="submit">Submit</button>
+     </form>
+     <div>
+            <h3>Submitted answer:</h3>
+            <p id="pAnswer">{{ inpValSubmitted }}</p>
+        </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+           colorInp: null,
+           inpValSubmitted: ''
+        }
+    },
+    methods: {
+        registerAnswer(){
+            if(this.colorInp){
+                this.inpValSubmitted = this.colorInp;
+            }
+                
+        }
+    }
+}
+</script>
+
+<style scoped>
+  div {
+    border: dashed black 1px;
+    border-radius: 10px;
+    padding: 0 20px 20px 20px;
+    margin-top: 20px;
+    display: inline-block;
+  }
+  button {
+    margin: 10px;
+    display: block;
+  }
+  #pAnswer {
+    background-color: lightgreen;
+    padding: 5px;
+  }
+</style>  
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import App from './App10.vue'
-import Foods from './components/Foods.vue'
-import Animals from './components/Animals.vue';
+import App from './App11.vue'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: '/animals', component: Animals},
-        {path: '/foods', component: Foods},
-    ]
-})
 
 const app = createApp(App)
-app.use(router)
 app.mount('#app')
